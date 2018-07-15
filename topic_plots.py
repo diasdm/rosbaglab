191b8d2c1e0a74c0cee9b975cb12b47307b3beff
David <david6dias@gmail.com>
2018-07-15 11:58:51 +0100

    Placed plot_num as global variable

--- topic_plots.py
+++ topic_plots.py
@@ -8,14 +8,17 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def plot_position_xsmth_multiysmth(plot_num, topic_list, topic_idx_list, title, xlabel, xfuncstr, ylabel, yfuncstr_l, axis_equal, legends_l=None):
+plot_num = 0
+
+def plot_position_xsmth_multiysmth(topic_list, topic_idx_list, title, xlabel, xfuncstr, ylabel, yfuncstr_l, axis_equal, legends_l=None):
     use_topic_legends = 0
     if legends_l == None:
         use_topic_legends = 1
     elif len(yfuncstr_l) != len(legends_l):
         print('The number of functions to plot must be the same as the number of legends!')
         return
-    fig = plt.figure('Figure ' + str(plot_num[0]))
+    global plot_num
+    fig = plt.figure('Figure ' + str(plot_num))
 
     plt.title(title)
     plt.xlabel(xlabel)
@@ -40,93 +43,89 @@ def plot_position_xsmth_multiysmth(plot_num, topic_list, topic_idx_list, title, 
         plt.axis('equal')
     plt.legend()
     plt.show()
-    plot_num[0] += 1
+    plot_num += 1
 
-def plot_position_x_y(plot_num, topic_list, topic_idx_list):
-    plot_position_xsmth_multiysmth(plot_num, topic_list, topic_idx_list, 'X, Y position', 'X position [m]', 'get_position_x', 'Y position [m]', ['get_position_y'], True)
+def plot_position_x_y(topic_list, topic_idx_list):
+    plot_position_xsmth_multiysmth(topic_list, topic_idx_list, 'X, Y position', 'X position [m]', 'get_position_x', 'Y position [m]', ['get_position_y'], True)
 
-def plot_position_time_ysmth(plot_num, topic_list, topic_idx_list, title, ylabel, yfuncstr_l):
-    plot_position_xsmth_multiysmth(plot_num, topic_list, topic_idx_list, title, 'Time [secs]', 'get_time', ylabel, yfuncstr_l, False)
+def plot_position_time_ysmth(topic_list, topic_idx_list, title, ylabel, yfuncstr_l):
+    plot_position_xsmth_multiysmth(topic_list, topic_idx_list, title, 'Time [secs]', 'get_time', ylabel, yfuncstr_l, False)
 
-def plot_position_time_multiysmth(plot_num, topic_list, topic_idx_list, title, ylabel, yfunc, ylegends):
-    plot_position_xsmth_multiysmth(plot_num, topic_list, topic_idx_list, title, 'Time [secs]', 'get_time', ylabel, yfunc, False, ylegends)
+def plot_position_time_multiysmth(topic_list, topic_idx_list, title, ylabel, yfunc, ylegends):
+    plot_position_xsmth_multiysmth(topic_list, topic_idx_list, title, 'Time [secs]', 'get_time', ylabel, yfunc, False, ylegends)
 
-def plot_position_time_x(plot_num, topic_list, topic_idx_list):
-    plot_position_time_ysmth(plot_num, topic_list, topic_idx_list, 'X position over time', 'X position [m]', ['get_position_x'])
+def plot_position_time_x(topic_list, topic_idx_list):
+    plot_position_time_ysmth(topic_list, topic_idx_list, 'X position over time', 'X position [m]', ['get_position_x'])
 
-def plot_position_time_y(plot_num, topic_list, topic_idx_list):
-    plot_position_time_ysmth(plot_num, topic_list, topic_idx_list, 'Y position over time', 'Y position [m]', ['get_position_y'])
+def plot_position_time_y(topic_list, topic_idx_list):
+    plot_position_time_ysmth(topic_list, topic_idx_list, 'Y position over time', 'Y position [m]', ['get_position_y'])
 
-def plot_position_time_z(plot_num, topic_list, topic_idx_list):
-    plot_position_time_ysmth(plot_num, topic_list, topic_idx_list, 'Z position over time', 'Z position [m]', ['get_position_z'])
+def plot_position_time_z(topic_list, topic_idx_list):
+    plot_position_time_ysmth(topic_list, topic_idx_list, 'Z position over time', 'Z position [m]', ['get_position_z'])
 
-def plot_orientation_roll(plot_num, topic_list, topic_idx_list):
-    plot_position_time_ysmth(plot_num, topic_list, topic_idx_list, 'Roll rotation over time', 'Roll rotation [deg]', ['get_roll'])
+def plot_orientation_roll(topic_list, topic_idx_list):
+    plot_position_time_ysmth(topic_list, topic_idx_list, 'Roll rotation over time', 'Roll rotation [deg]', ['get_roll'])
 
-def plot_orientation_pitch(plot_num, topic_list, topic_idx_list):
-    plot_position_time_ysmth(plot_num, topic_list, topic_idx_list, 'Pitch rotation over time', 'Pitch rotation [deg]', ['get_pitch'])
+def plot_orientation_pitch(topic_list, topic_idx_list):
+    plot_position_time_ysmth(topic_list, topic_idx_list, 'Pitch rotation over time', 'Pitch rotation [deg]', ['get_pitch'])
 
-def plot_orientation_yaw(plot_num, topic_list, topic_idx_list):
-    plot_position_time_ysmth(plot_num, topic_list, topic_idx_list, 'Yaw rotation over time', 'Yaw rotation [deg]', ['get_yaw'])
+def plot_orientation_yaw(topic_list, topic_idx_list):
+    plot_position_time_ysmth(topic_list, topic_idx_list, 'Yaw rotation over time', 'Yaw rotation [deg]', ['get_yaw'])
 
-def plot_orientation(plot_num, topic_list, topic_idx_list):
-    plot_orientation_roll(plot_num, topic_list, topic_idx_list)
-    plot_orientation_pitch(plot_num, topic_list, topic_idx_list)
-    plot_orientation_yaw(plot_num, topic_list, topic_idx_list)
+def plot_orientation(topic_list, topic_idx_list):
+    plot_orientation_roll(topic_list, topic_idx_list)
+    plot_orientation_pitch(topic_list, topic_idx_list)
+    plot_orientation_yaw(topic_list, topic_idx_list)
 
-def plot_linear_acceleration(plot_num, topic_list, topic_idx_list):
-    plot_position_time_ysmth(plot_num, topic_list, topic_idx_list, 'X linear accelerarion over time', 'X linear accelerarion [m/s^2]', ['get_linear_acceleration_x'])
-    plot_position_time_ysmth(plot_num, topic_list, topic_idx_list, 'Y linear accelerarion over time', 'Y linear accelerarion [m/s^2]', ['get_linear_acceleration_y'])
-    plot_position_time_ysmth(plot_num, topic_list, topic_idx_list, 'Z linear accelerarion over time', 'Z linear accelerarion [m/s^2]', ['get_linear_acceleration_z'])
+def plot_linear_acceleration(topic_list, topic_idx_list):
+    plot_position_time_ysmth(topic_list, topic_idx_list, 'X linear accelerarion over time', 'X linear accelerarion [m/s^2]', ['get_linear_acceleration_x'])
+    plot_position_time_ysmth(topic_list, topic_idx_list, 'Y linear accelerarion over time', 'Y linear accelerarion [m/s^2]', ['get_linear_acceleration_y'])
+    plot_position_time_ysmth(topic_list, topic_idx_list, 'Z linear accelerarion over time', 'Z linear accelerarion [m/s^2]', ['get_linear_acceleration_z'])
 
-def plot_angular_velocity(plot_num, topic_list, topic_idx_list):
-    plot_position_time_ysmth(plot_num, topic_list, topic_idx_list, 'X angular velocity over time', 'X angular velocity [deg/s^2]', ['get_angular_velocity_x'])
-    plot_position_time_ysmth(plot_num, topic_list, topic_idx_list, 'Y angular velocity over time', 'Y angular velocity [deg/s^2]', ['get_angular_velocity_y'])
-    plot_position_time_ysmth(plot_num, topic_list, topic_idx_list, 'Z angular velocity over time', 'Z angular velocity [deg/s^2]', ['get_angular_velocity_z'])
+def plot_angular_velocity(topic_list, topic_idx_list):
+    plot_position_time_ysmth(topic_list, topic_idx_list, 'X angular velocity over time', 'X angular velocity [deg/s^2]', ['get_angular_velocity_x'])
+    plot_position_time_ysmth(topic_list, topic_idx_list, 'Y angular velocity over time', 'Y angular velocity [deg/s^2]', ['get_angular_velocity_y'])
+    plot_position_time_ysmth(topic_list, topic_idx_list, 'Z angular velocity over time', 'Z angular velocity [deg/s^2]', ['get_angular_velocity_z'])
 
-def plot_position(plot_num, topic_list, topic_idx_list):
-    plot_position_x_y(plot_num, topic_list, topic_idx_list)
-    plot_position_time_x(plot_num, topic_list, topic_idx_list)
-    plot_position_time_y(plot_num, topic_list, topic_idx_list)
+def plot_position(topic_list, topic_idx_list):
+    plot_position_x_y(topic_list, topic_idx_list)
+    plot_position_time_x(topic_list, topic_idx_list)
+    plot_position_time_y(topic_list, topic_idx_list)
 
-def plot_odometry(plot_num, topic_list, topic_idx_list):
-    plot_position(plot_num, topic_list, topic_idx_list)
-    plot_orientation(plot_num, topic_list, topic_idx_list)
+def plot_odometry(topic_list, topic_idx_list):
+    plot_position(topic_list, topic_idx_list)
+    plot_orientation(topic_list, topic_idx_list)
 
-def plot_imu(plot_num, topic_list, topic_idx_list):
-    plot_orientation(plot_num, topic_list, topic_idx_list)
-    plot_linear_acceleration(plot_num, topic_list, topic_idx_list)
-    plot_angular_velocity(plot_num, topic_list, topic_idx_list)
+def plot_imu(topic_list, topic_idx_list):
+    plot_orientation(topic_list, topic_idx_list)
+    plot_linear_acceleration(topic_list, topic_idx_list)
+    plot_angular_velocity(topic_list, topic_idx_list)
 
-def plot_latitude_time(plot_num, topic_list, topic_idx_list):
-    plot_position_time_ysmth(plot_num, topic_list, topic_idx_list, 'Latitude over time', 'Latitude [deg]', ['get_latitude'])
+def plot_latitude_time(topic_list, topic_idx_list):
+    plot_position_time_ysmth(topic_list, topic_idx_list, 'Latitude over time', 'Latitude [deg]', ['get_latitude'])
 
-def plot_longitude_time(plot_num, topic_list, topic_idx_list):
-    plot_position_time_ysmth(plot_num, topic_list, topic_idx_list, 'Longitude over time', 'Longitude [deg]', ['get_longitude'])
+def plot_longitude_time(topic_list, topic_idx_list):
+    plot_position_time_ysmth(topic_list, topic_idx_list, 'Longitude over time', 'Longitude [deg]', ['get_longitude'])
 
-def plot_gps_coordinates(plot_num, topic_list, topic_idx_list):
-    plot_position_xsmth_multiysmth(plot_num, topic_list, topic_idx_list, 'GPS coordinates', 'Latitude [deg]', 'get_latitude', 'Longitude [deg]', ['get_longitude'], True)
+def plot_gps_coordinates(topic_list, topic_idx_list):
+    plot_position_xsmth_multiysmth(topic_list, topic_idx_list, 'GPS coordinates', 'Latitude [deg]', 'get_latitude', 'Longitude [deg]', ['get_longitude'], True)
 
 def plot_topics_list(topics_list):
-    plot_num = [0]
+    plot_position(topics_list, range(len(topics_list)))
+    plot_orientation(topics_list, range(len(topics_list)))
+    plot_linear_acceleration(topics_list, range(len(topics_list)))
+    plot_angular_velocity(topics_list, range(len(topics_list)))
 
-    plot_position(plot_num, topics_list, range(len(topics_list)))
-    plot_orientation(plot_num, topics_list, range(len(topics_list)))
-    plot_linear_acceleration(plot_num, topics_list, range(len(topics_list)))
-    plot_angular_velocity(plot_num, topics_list, range(len(topics_list)))
-
-def plot_by_type(plot_num, topic_list, topic_idx_list):
+def plot_by_type(topic_list, topic_idx_list):
     type = topic_list[topic_idx_list[0]].type
 
     if type == 'nav_msgs/Odometry':
-        plot_odometry(plot_num, topic_list, topic_idx_list)
+        plot_odometry(topic_list, topic_idx_list)
     elif type == 'sensor_msgs/Imu':
-        plot_imu(plot_num, topic_list, topic_idx_list)
+        plot_imu(topic_list, topic_idx_list)
     else:
         print('ERROR, no plot method for ' + type)
 
 def plot_same_type(topic_list, bagprop):
-    plot_num = [0]
-
     for topic_idx_list in bagprop.diff_topic_idx:
-        plot_by_type(plot_num, topic_list, topic_idx_list)
+        plot_by_type(topic_list, topic_idx_list)
